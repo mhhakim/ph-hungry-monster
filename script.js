a3234c130bb1ae00e063eb57d2660be606010a23
Md Abdul Hakim <mail@hakim.one>
2021-02-23 19:09:03 +0600

    Code optimization

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
+function searchMeal() {
+    const searchQuery = document.getElementById('form-search').value;
+    let div = document.getElementById('meal-list');
+    div.innerHTML = `<div class="col-md-12 text-center alert alert-info">Searching...</div>`;
+    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' + searchQuery)
+        .then(response => response.json())
+        .then(meals => displaySearchResult(meals['meals']));
+}
+
+function displaySearchResult(meals) {
+    let div = document.getElementById('meal-list');
+    div.innerHTML = '';
+    if (meals != null) {
+        meals.forEach(meal => {
+            const elementBody = `
+            <div onclick="mealDetails(${meal.idMeal})" class="card">
+                <img src="${meal.strMealThumb}" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title text-center">${meal.strMeal}</h5>
+                </div>
+            </div>
+            `;
+            let element = document.createElement('div');
+            element.innerHTML = elementBody;
+            element.className = 'col-md-3';
+            div.appendChild(element);
+        });
+    } else {
+        div.innerHTML = `<div class="col-md-12 text-center alert alert-danger">No result found. Please try with different keyword.</div>`;
+    }
+}
+
 function mealDetails(id) {
     console.log('clicked')
     fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + id)
-    .then(response => response.json())
-    .then(data => displayMealDetails(data['meals'][0]));
+        .then(response => response.json())
+        .then(data => displayMealDetails(data['meals'][0]));
 }
 
 function displayMealDetails(data) {
@@ -19,52 +51,12 @@ function displayMealDetails(data) {
 
         let ingredientMeasure = 'strMeasure' + i;
         ingredientMeasure = data[ingredientMeasure];
-        
+
         if (ingredientName != null && ingredientName != "") {
             let p = document.createElement('p');
             p.innerHTML = `<p> <img src="./assets/tick.svg"> ${ingredientMeasure} ${ingredientName}</p>`;
             let div = document.getElementById('meal-details');
             div.appendChild(p);
         }
-    
-    }
-}
-
-
-
-
-// Set a default result
-searchMeal('');
-
-function searchMeal() {
-    const searchQuery = document.getElementById('form-search').value;
-    const url = 'https://www.themealdb.com/api/json/v1/1/search.php?s=' + searchQuery;
-    let div = document.getElementById('meal-list');
-    div.innerHTML = `<div class="col-md-12 text-center alert alert-info">Searching...</div>`;
-    fetch(url)
-        .then(response => response.json())
-        .then(meals => displaySearchResult(meals['meals']));
-}
-
-function displaySearchResult(meals) {
-    let div = document.getElementById('meal-list');
-    div.innerHTML = '';
-    if (meals != null) {
-        meals.forEach(meal => {
-            let elementBody = `
-            <div onclick="mealDetails(${meal.idMeal})" class="card">
-                <img src="${meal.strMealThumb}" class="card-img-top">
-                <div class="card-body">
-                    <h5 class="card-title text-center">${meal.strMeal}</h5>
-                </div>
-            </div>
-            `;
-            let element = document.createElement('div');
-            element.innerHTML = elementBody;
-            element.className = 'col-md-3';
-            div.appendChild(element);
-        });
-    } else {
-        div.innerHTML = `<div class="col-md-12 text-center alert alert-danger">No result found. Please try with different keyword.</div>`;
     }
 }
